5e9d56a189fea2c607a8c36308681068311ba9be
Chris Neal <chrisvneal@gmail.com>
2018-10-07 13:19:58 -1000

    Included alt for images

--- js/main.js
+++ js/main.js
@@ -163,7 +163,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = `${restaurant.name}`;  
+  image.alt = `${restaurant.name}'s restaurant`;  
   li.append(image);
 
   const name = document.createElement('h1');
